b38ea9d834a9e83450f571e2165a835678f5cae0
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-05-02 10:11:46 +0300

    Create sprinf cloud config server

--- limits-service/src/main/java/com/web/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/web/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,14 +1,18 @@
 package com.web.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
-		return new LimitConfiguration(1,1000);
+		return new LimitConfiguration(configuration.getMinimum(),configuration.getMaximum());
 	}
 
 }
